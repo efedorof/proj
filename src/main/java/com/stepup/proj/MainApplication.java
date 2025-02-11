4eb1098f45af751115a1f7e9652290b8067ddaf0
Eugene Fedorov <efedorof@gmail.com>
2025-02-11 21:08:37 +0300

    Улучшили логику main и doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -1,14 +1,17 @@
 package com.stepup.proj;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("1111");
+        System.out.println("---");
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
