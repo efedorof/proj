27831825aa3753c9139b015146c78573d476af8f
Eugene Fedorov <efedorof@gmail.com>
2025-02-11 20:08:02 +0300

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,4 +5,9 @@ public class MainApplication {
         System.out.println("Hello, World!");
         System.out.println("1111");
     }
+
+    public static void doSomething() {
+        System.out.println(1);
+
+    }
 }
