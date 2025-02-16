05db18f161a125ea6c1bdf16792740c258214489
Eugene Fedorov <efedorof@gmail.com>
2025-02-16 20:01:49 +0300

    Добавлен вызов метода doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -9,6 +9,7 @@ public class MainApplication {
         System.out.println("---");
         System.out.println("Hello, World!");
         System.out.println("1111");
+        doSomething();
         System.out.println("---");
     }
 
